be4c3aef557a0d0de02beeb2ac12e32ac0098718
FlorianMMI <florian.bounissou@etu.unlim.fr>
2025-01-27 10:17:32 +0100

    Enhance sphere geometry and material properties; update light position and add initial README content

--- exercice1/exo1.js
+++ exercice1/exo1.js
@@ -2,22 +2,33 @@ import * as THREE from 'three';
 
 
 // Scene
+// 1. Racine, va contenir tous les objets de la scène, les lumières, les ou la caméras 
 const scene = new THREE.Scene();
 
 
 // Sphere
-const geometry = new THREE.SphereGeometry(3, 16, 16);
-const material = new THREE.MeshBasicMaterial({
-   color: 0xffffff,
-   wireframe: true,
-});
+// 2. Création d'une sphère avec un rayon de 3, 16 segments de subdivisions
+// 3. Affichage du wireframe de la frame de la sphère
+const geometry = new THREE.SphereGeometry(4, 32, 32);
+// const material = new THREE.MeshLambertMaterial({
+//    color: 0xff0000,
+//    emissive: 0x000000,
+//    emissiveIntensity: 1,
+//    lights: true
+// });
+const material = new THREE.MeshPhongMaterial({
+   color: 0xff0000,
+   specular: 0xffffff,
+   shininess: 200,
+   lights: true
+ });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
 
 // Light
 const light = new THREE.PointLight(0xffffff, 1, 100);
-light.position.set(0, 10, 10);
+light.position.set(30, 10, 10);
 scene.add(light);
 const aLight = new THREE.AmbientLight(0x151515);
 scene.add(aLight);
