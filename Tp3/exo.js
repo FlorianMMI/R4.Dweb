df13909574ef76587b51b5f785a8401f1fd2d1ae
FlorianMMI <florian.bounissou@etu.unlim.fr>
2025-02-05 08:23:02 +0100

    ça marcher aps

---
+++ Tp3/exo.js
@@ -0,0 +1,102 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import GUI from 'https://cdn.jsdelivr.net/npm/lil-gui@0.20/+esm';
+import Stats from 'three/addons/libs/stats.module.js';
+
+const container = document.querySelector('.container');
+const stats = new Stats();
+container.appendChild(stats.dom);
+
+
+const scene = new THREE.Scene();
+
+scene.background = new THREE.CubeTextureLoader()
+.setPath('./assets/')
+.load([
+  'posx.jpg',
+  'negx.jpg',
+  'posy.jpg',
+  'negy.jpg',
+  'posz.jpg',
+  'negz.jpg'
+
+])
+
+const gui = new GUI();
+gui.add (document, "title");
+
+let box = new THREE.BoxGeometry(1.5, 3, 0.5);
+let boxMat = new THREE.MeshBasicMaterial({color: 0xa9a9a9});
+let boxMesh = new THREE.Mesh(box, boxMat);
+boxMesh.castShadow = true;
+boxMesh.receiveShadow = false;
+boxMesh.position.set(0, 2, 0);
+scene.add(boxMesh);
+
+let sol = new THREE.PlaneGeometry(10, 10);
+let solMat = new THREE.MeshBasicMaterial({color: 0xd3d3d3});
+let solMesh = new THREE.Mesh(sol, solMat);
+solMesh.receiveShadow = true;
+solMesh.rotateX(-Math.PI / 2); 
+scene.add(solMesh);
+
+// On ne voit pas en dessous car la lumière arrive du haut
+
+
+let light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+light.position.set(50, 100, 10);
+light.target.position.set(0, 0, 0);
+light.castShadow = true;
+scene.add(light);
+scene.add(new THREE.DirectionalLightHelper(light));
+
+
+light.shadow.mapSize.width = 512;
+light.shadow.mapSize.height = 512;
+light.shadow.camera.near = 0.5;
+light.shadow.camera.far = 500;
+
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight);
+camera.position.set(0, 50, 0);
+
+scene.add(camera);
+
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
+
+
+
+
+// scene.add(new THREE.AxesHelper(10));
+const gridHelper = new THREE.GridHelper(25, 25);
+scene.add(gridHelper);
+// scene.add(new THREE.SpotLightHelper(light));
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+});
+
+
+
+const controls = new OrbitControls(camera, canvas);
+
+controls.enableDamping = true;
+// let time = 0.01;
+const loop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+  stats.update();
+}
+
+
+loop();
